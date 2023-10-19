75c6ff5e7a1ec9a98eba56d21d722fa48445bb90
Devsoft <by-JohnChen@users.noreply.github.com>
2023-10-20 01:15:01 +0800

    Update fdqms.sh

--- fdqms.sh
+++ fdqms.sh
@@ -1,6 +1,6 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
-tar -xvf nanominer-linux-3.5.2.tar.gz
-cd nanominer-linux-3.5.2
+wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+tar -xvf nanominer-linux-3.8.5.tar.gz
+cd nanominer-linux-3.8.5
 rm -rf termt.ini
-wget -O termt.ini https://raw.githubusercontent.com/fatih-dogmus/luckpool/main/termt.ini
+wget -O termt.ini https://raw.githubusercontent.com/by-JohnChen/luckpool/main/termt.ini
 ./nanominer termt.ini
